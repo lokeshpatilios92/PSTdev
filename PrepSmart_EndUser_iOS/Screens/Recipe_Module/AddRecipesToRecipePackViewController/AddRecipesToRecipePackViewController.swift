e1b472b715d5e6fd847fbb16a681a8b59c360c8b
JitendraPDev <38884078+shubhammayankgupta@users.noreply.github.com>
2022-04-16 17:42:21 +0530

    added recepe API

--- PrepSmart_EndUser_iOS/Screens/Recipe_Module/AddRecipesToRecipePackViewController/AddRecipesToRecipePackViewController.swift
+++ PrepSmart_EndUser_iOS/Screens/Recipe_Module/AddRecipesToRecipePackViewController/AddRecipesToRecipePackViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 protocol DataEnteredDelegate: class {
     func sendFlag(info: Int)
+    func didDataAddedToList(at indexPath: IndexPath?, receipes: [Recipes])
 }
 
 class AddRecipesToRecipePackViewController: BaseViewController {
@@ -19,6 +20,10 @@ class AddRecipesToRecipePackViewController: BaseViewController {
     @IBOutlet weak var btn_Cancel: UIButton!
     
     weak var delegate: DataEnteredDelegate? = nil
+    
+    private var recepies = [Recipes]()
+    private var selectedReceipes = [Recipes]()
+    var indexPath: IndexPath?
     
     let addRecipesToRecipeTableCell = "AddRecipesToRecipeTableCell"
     let titleTableViewCell          = "TitleTableViewCell"
@@ -45,12 +50,14 @@ class AddRecipesToRecipePackViewController: BaseViewController {
         
         self.btn_Done.addTarget(self, action: #selector(onClickDoneButton(sender:)), for: .touchUpInside)
         self.btn_Cancel.addTarget(self, action: #selector(onClickCancelButton(sender:)), for: .touchUpInside)
+        self.getReceipeList()
     }
     
     
     @objc func onClickDoneButton(sender : UIButton)
     {
         delegate?.sendFlag(info: 1)
+        delegate?.didDataAddedToList(at: indexPath, receipes: selectedReceipes)
         self.navigationController?.popViewController(animated: true)
     }
     
@@ -69,9 +76,11 @@ extension AddRecipesToRecipePackViewController : UITableViewDataSource, UITableV
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 0 {
-            return 5
+            return self.recepies.count
+        } else {
+            return self.selectedReceipes.count
         }
-        return 4
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -81,17 +90,34 @@ extension AddRecipesToRecipePackViewController : UITableViewDataSource, UITableV
         cell.bgView.backgroundColor = .white
         
         if indexPath.section == 0 {
+            let recipe = self.recepies[indexPath.row]
             cell.view_qty.isHidden = false
-            cell.lbl_servings.text = "Servings"
+            cell.lbl_servings.text = ""
             cell.btn_addORdelete.setImage(#imageLiteral(resourceName: "add-image"), for: .normal)
+            
+            cell.itemTitleLabel.text = recipe.recipe_name
+            cell.itemInfoLable.text = "\(recipe.protein ?? 0))P. \(recipe.carbohydrates ?? 0)C"
+            
+            cell.lbl_quantity.text = "\(recipe.totalQuantity ?? 0)"
+            cell.btn_addORdelete.addTarget(self, action: #selector(btnAddOrDeleteAction(_:)), for: .touchUpInside)
         }
         else
         {
             cell.view_qty.isHidden = true
-            cell.lbl_servings.text = "2 Servings"
+            cell.lbl_servings.text = ""
             cell.btn_addORdelete.setImage(#imageLiteral(resourceName: "delete_red"), for: .normal)
+            let recipe = self.selectedReceipes[indexPath.row]
+            
+            cell.itemTitleLabel.text = recipe.recipe_name
+            cell.itemInfoLable.text = "\(recipe.protein ?? 0))P. \(recipe.carbohydrates ?? 0)C"
+            
+            cell.lbl_quantity.text = "\(recipe.totalQuantity ?? 0)"
         }
         cell.contentView.backgroundColor = .groupTableViewBackground
+        
+        cell.btn_plus.addTarget(self, action: #selector(btnPlusAction(_:)), for: .touchUpInside)
+        cell.btn_minus.addTarget(self, action: #selector(btnMinusAction(_:)), for: .touchUpInside)
+       
         return cell
         
     }
@@ -130,6 +156,50 @@ extension AddRecipesToRecipePackViewController : UITableViewDataSource, UITableV
 //        return 100
 //    }
     
+    @objc func btnPlusAction(_ sender: UIButton) {
+        guard let cell = sender.superview?.superview?.superview?.superview?.superview as? AddRecipesToRecipeTableCell,
+              let indexPath = tableView.indexPath(for: cell) else { return }
+        
+        if indexPath.section == 0 {
+            var totlQuantity = recepies[indexPath.row].totalQuantity ?? 0
+            totlQuantity = totlQuantity + 1
+            recepies[indexPath.row].totalQuantity = totlQuantity
+            self.tableView.reloadData()
+        }
+    }
+    
+    @objc func btnMinusAction(_ sender: UIButton) {
+        guard let cell = sender.superview?.superview?.superview?.superview?.superview as? AddRecipesToRecipeTableCell,
+              let indexPath = tableView.indexPath(for: cell) else { return }
+        if indexPath.section == 0 {
+            var totlQuntity = recepies[indexPath.row].totalQuantity ?? 0
+            if totlQuntity > 0 {
+                totlQuntity = totlQuntity - 1
+                recepies[indexPath.row].totalQuantity = totlQuntity
+            }
+            self.tableView.reloadData()
+        }
+    }
+    
+    @objc func btnAddOrDeleteAction(_ sender: UIButton) {
+        guard let cell = sender.superview?.superview?.superview as? AddRecipesToRecipeTableCell,
+              let indexPath = tableView.indexPath(for: cell) else { return }
+        
+        if indexPath.section == 0 {
+            let recipe = recepies[indexPath.row]
+            if let index = self.selectedReceipes.firstIndex(where: { $0.recipe_id == recipe.recipe_id }) {
+                self.selectedReceipes.remove(at: index)
+            }
+            self.selectedReceipes.append(recipe)
+        } else {
+            self.selectedReceipes.remove(at: indexPath.row)
+        }
+        
+        self.tableView.reloadData()
+       
+    }
+    
+    
     func viewForHeaderInSection(section:Int) -> UIView {
         if section == 0 {
             let footerView = UIView()
@@ -142,6 +212,52 @@ extension AddRecipesToRecipePackViewController : UITableViewDataSource, UITableV
             cell.lbl_title.textAlignment = .center
             cell.contentView.backgroundColor = .white
             return cell.contentView
+        }
+    }
+    
+    
+    func getReceipeList() {
+        Loader.sharedInstance.showIndicator()
+        
+        Api_Http_Class.shareinstance.AlemfFireRowAPICall(methodName: Constants.getRecipeList, params: nil , method: .get) { (result) in
+            switch result
+            {
+            case .success(let data, let dictionary):
+                
+                if let dict : NSDictionary = dictionary as? NSDictionary
+                {
+                    if let status = dict["status"] as? Bool, status == true
+                    {
+                        Loader.sharedInstance.hideIndicator()
+                        do {
+                            if let receipeList = dict["recipe_list"] as? [[String: Any]], let data = try? JSONSerialization.data(withJSONObject: receipeList, options: [.prettyPrinted]) {
+                                self.recepies = try JSONDecoder().decode([Recipes].self, from: data)
+                                self.tableView.reloadData()
+                            }
+
+                        }
+                        catch (let error){
+                            Alert.show(vc: self, titleStr: AMPLocalizeUtils.defaultLocalizer.stringForKey(key: Alert.kTitle), messageStr: error.localizedDescription)
+                        }
+                    }
+                    else
+                    {
+                        Alert.show(vc: self, titleStr: AMPLocalizeUtils.defaultLocalizer.stringForKey(key: Alert.kTitle), messageStr: "")
+                    }
+                }
+                else
+                {
+                    Alert.show(vc: self, titleStr: AMPLocalizeUtils.defaultLocalizer.stringForKey(key: Alert.kTitle), messageStr: "")
+                }
+                Loader.sharedInstance.hideIndicator()
+                break
+                
+            case .failer(let error):
+                
+                Alert.show(vc: self, titleStr: AMPLocalizeUtils.defaultLocalizer.stringForKey(key: Alert.kTitle), messageStr: error.localizedDescription)
+                Loader.sharedInstance.hideIndicator()
+                break
+            }
         }
     }
 }
