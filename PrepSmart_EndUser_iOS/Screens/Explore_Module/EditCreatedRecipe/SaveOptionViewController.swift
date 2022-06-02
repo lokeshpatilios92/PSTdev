249b2abd9f96f0738540f2f6ca9bd24fb965b1b0
Shubham Gupta <shubhammayankgupta@gmail.com>
2022-06-03 00:42:43 +0530

    code updated

--- PrepSmart_EndUser_iOS/Screens/Explore_Module/EditCreatedRecipe/SaveOptionViewController.swift
+++ PrepSmart_EndUser_iOS/Screens/Explore_Module/EditCreatedRecipe/SaveOptionViewController.swift
@@ -14,6 +14,9 @@ class SaveOptionViewController: BaseViewController {
     
     @IBOutlet var btn_save: UIButton!
     @IBOutlet var btn_cancel: UIButton!
+    
+    var cancelTap: (() -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,6 +32,12 @@ class SaveOptionViewController: BaseViewController {
         btn_cancel.layer.borderWidth = 1.0
         btn_cancel.layer.borderColor = UIColor.appOrangeColor().cgColor
     }
+    
+    
+    @IBAction func btnCancelAction(_ sender: Any) {
+        self.cancelTap!()
+    }
+    
     /*
     // MARK: - Navigation
 
